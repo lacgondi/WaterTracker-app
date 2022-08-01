0a62b5279a9974b5a229a7d27d1266ee90348751
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-01 18:27:58 +0200

    Add form.

--- lib/initial.dart
+++ lib/initial.dart
@@ -1,3 +1,4 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
 
 class InitialSet extends StatefulWidget {
@@ -14,6 +15,46 @@ class InitialSetState extends State<InitialSet> {
     var goal;
   }
 
+  Form _initalSet() {
+    final _formKey = GlobalKey<FormState>();
+
+    return Form(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+                padding: EdgeInsets.all(18.0),
+                child: TextFormField(
+                    enableInteractiveSelection: false,
+                    decoration:
+                        InputDecoration(labelText: "Water drinking goal"),
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next)),
+            Padding(
+                padding: EdgeInsets.all(18.0),
+                child: TextFormField(
+                    enableInteractiveSelection: false,
+                    decoration: InputDecoration(labelText: "Container size"),
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.next)),
+            Padding(
+              padding: EdgeInsets.all(18.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ),
+          ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +62,7 @@ class InitialSetState extends State<InitialSet> {
         title: Text('Water TrackR'),
         backgroundColor: Colors.teal,
       ),
-      body: Center(
-          child: Text(
-        'The form will come here soon',
-        style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic),
-      )),
+      body: _initalSet(),
     );
   }
 }
