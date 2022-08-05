0fcf03178949405e1190f55893ffb5d8ca366b35
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-05 12:36:29 +0200

    Add water drop render to tracking.dart

--- lib/tracking.dart
+++ lib/tracking.dart
@@ -8,9 +8,20 @@ class Tracking extends StatefulWidget {
 }
 
 class TrackingState extends State<Tracking> {
+  Row _buildRow(int g, int c) {
+    List<IconData> bottles = <IconData>[];
+    for (var i = 0; i < g ~/ c; i++) {
+      bottles.add(Icons.water_drop_outlined);
+    }
+    return Row(children: <Widget>[for (var item in bottles) Icon(item)]);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tracking')),
+      appBar: AppBar(
+        title: Text('Tracking'),
+      ),
+      body: _buildRow(goal, containerSize),
     );
   }
 }
