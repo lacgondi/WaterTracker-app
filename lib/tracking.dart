c62e4caae38334b5e7f8169ada596c29734817ab
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-05 13:07:30 +0200

    Add clickable icons to tracking.dart

--- lib/tracking.dart
+++ lib/tracking.dart
@@ -8,12 +8,32 @@ class Tracking extends StatefulWidget {
 }
 
 class TrackingState extends State<Tracking> {
+
+
+  bool isPressed = false;
+
   Row _buildRow(int g, int c) {
     List<IconData> bottles = <IconData>[];
     for (var i = 0; i < g ~/ c; i++) {
       bottles.add(Icons.water_drop_outlined);
     }
-    return Row(children: <Widget>[for (var item in bottles) Icon(item)]);
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          for (var item in bottles)
+            IconButton(
+                icon: (isPressed) ? Icon(item) : Icon(Icons.water_drop),
+                color: Colors.blueAccent[200],
+                iconSize: 55.0,
+                onPressed: () => onTap()),
+        ]);
+  }
+
+  void onTap() {
+    setState(() {
+      isPressed = !isPressed;
+    });
   }
 
   Widget build(BuildContext context) {
