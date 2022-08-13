b8d20e9f6c46066dc4ce2c1b93bcb095d8542b8b
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-08-13 12:34:04 +0200

    Add form and button to settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'dart:core';
 
+import 'package:water_tracker/tracking.dart';
+
+
+
 class Settings extends StatefulWidget {
   const Settings({super.key});
   static const id = "/settings";
@@ -9,11 +13,47 @@ class Settings extends StatefulWidget {
   State<Settings> createState() => SettingsSetState();
 }
 
-Widget _buildBody() {
-  return Text('soon');
-}
 
 class SettingsSetState extends State<Settings> {
+  TextEditingController goalSettingC = TextEditingController();
+  TextEditingController containerSettingC = TextEditingController();
+  
+Widget _buildBody() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+            padding: EdgeInsets.only(left: 18.0, right: 18.0),
+            child: TextField(
+              controller: goalSettingC,
+              enableInteractiveSelection: false,
+              decoration:
+                  InputDecoration(labelText: "Change water intake goal"),
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+            )),
+        Padding(
+            padding: EdgeInsets.only(left: 18.0, right: 18.0),
+            child: TextField(
+              controller: containerSettingC,
+              enableInteractiveSelection: false,
+              decoration: InputDecoration(labelText: "Change container size"),
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+            )),
+        Padding(
+          padding: EdgeInsets.all(18.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, Tracking.id);
+            },
+            child: const Text('Submit'),
+          ),
+        ),
+      ],
+    );
+}
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Settings")),
